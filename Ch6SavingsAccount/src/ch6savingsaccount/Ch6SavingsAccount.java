7da574532bdeff8d74a0a99fd32942ce17369fd9
Kevin Bell <bellKevin@pm.me>
2022-06-01 14:27:42 +0000

    Update Ch6SavingsAccount.java

--- Ch6SavingsAccount/src/ch6savingsaccount/Ch6SavingsAccount.java
+++ Ch6SavingsAccount/src/ch6savingsaccount/Ch6SavingsAccount.java
@@ -11,6 +11,8 @@ public class Ch6SavingsAccount {
         int cycles;
         double interestRate;
         double balance;
+        
+        //get the needed data
         System.out.print("Enter starting balance of this Savings Account: ");
         balance = computerKeyboardInput.nextDouble();
         System.out.println("Interest rate: to enter 2.5%, type 2.5");
@@ -18,8 +20,12 @@ public class Ch6SavingsAccount {
         interestRate = computerKeyboardInput.nextDouble();
         System.out.print("Enter number of cycles to grow this Savings Account: ");
         cycles = computerKeyboardInput.nextInt();
+        
+        //set those values in the account object
         savingsAccount.setInterestRate(interestRate);
         savingsAccount.setBalance(balance);
+        
+        //now grow that account
         savingsAccount.growAccount(cycles);
         System.out.printf("After %d cycles, the account is worth $%-,10.2f\n", cycles, savingsAccount.getBalance());
     }
